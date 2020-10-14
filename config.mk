f9a7dfca435b4d168ea69b63eb1ddc58181b4b5c
0ranko0P <ranko0p@outlook.com>
2020-10-14 19:47:44 +0800

    pixelstyle: Drop ime system_lm_dir prop

--- config.mk
+++ config.mk
@@ -34,8 +34,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Gboard configuration
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.bs_theme=true \
-    ro.com.google.ime.theme_id=5 \
-    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+    ro.com.google.ime.theme_id=5
 
 # SetupWizard configuration
 PRODUCT_PRODUCT_PROPERTIES += \
